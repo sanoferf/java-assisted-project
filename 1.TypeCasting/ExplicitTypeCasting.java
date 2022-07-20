fe0b27d929fdaf44bab5c95a9d9e843e678ea7a9
sanoferf <sanofarzh@gmail.com>
2022-07-20 13:25:38 +0530

    Assisted project

---
+++ 1.TypeCasting/ExplicitTypeCasting.java
@@ -0,0 +1,26 @@
+package com.simplilearn.demo2;
+
+public class ExplicitTypeCasting {
+public static void main(String [] args) {
+
+		
+
+		double a= 23.67;
+
+		
+
+		int b=(int) a;  ///convert forcefully to int
+
+		
+
+		System.out.println("Converted Double "+a+" to int "+b);
+
+		 
+
+	}
+
+
+
+}
+
+
---
+++ 7.InnerClass/LocalInnerClass.java
@@ -0,0 +1,82 @@
+package com.simplilearn.innerclass;
+
+public class LocalInnerClass {
+
+
+		
+
+		void Check(String locker_key) {
+
+			
+
+			
+
+			if(locker_key.equals("sanofer@gmail.com")) {
+
+				
+
+				class Inner {
+
+					
+
+					
+
+					void validate() {
+
+						System.out.println("User Found and Authenticated");
+
+					}
+
+					
+
+					
+
+				}
+
+				
+
+				Inner inner= new Inner();
+
+				inner.validate();
+
+
+
+
+
+			}
+
+			else {
+
+				
+
+				System.out.println("Not Valid");
+
+			}
+
+			
+
+			
+
+		}
+
+		
+
+		
+
+		public static void main(String[] args) {
+
+			
+
+			LocalInnerClass outer= new LocalInnerClass();
+
+			outer.Check("sanofer@gmail.com");
+
+			
+
+			
+
+		}
+
+	}
+
+

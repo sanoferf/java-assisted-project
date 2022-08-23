b0f67de3955437c12ca0e7860e2df5791c2d0a66
sanoferf <sanofarzh@gmail.com>
2022-08-23 11:31:44 +0530

    phase3

---
+++ phase3 ass/SpringDemo/src/main/java/com/simplilearn/demo/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.simplilearn.demo;
+
+
+
+public class HelloWorld {
+
+	
+
+	public void print() {
+
+		System.out.println("Welcome to Spring Framework");
+
+	}
+
+
+
+}
---
+++ phase3 ass/Springappdemo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+
+public class HelloController {
+
+	
+
+	@GetMapping("/testme")
+
+	public String hello() {
+
+		return "Hello ! Welcome to SpringBoot";
+
+	}
+
+
+
+}
